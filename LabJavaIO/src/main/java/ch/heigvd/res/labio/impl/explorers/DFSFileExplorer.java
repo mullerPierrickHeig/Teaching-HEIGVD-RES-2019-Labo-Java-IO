309f1f393e894e71d88785cfb4082966a21b0cf2
Pierrick Muller <pierrick.muller@heig-vd.ch>
2019-03-13 09:44:16 +0100

    Test d'un tri des fichiers/dossier enfants dans la méthode explore

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -29,6 +30,10 @@ public class DFSFileExplorer implements IFileExplorer {
     if(rootDirectory.isDirectory())
     {
       File[] childs = rootDirectory.listFiles();
+
+      Arrays.sort(childs);
+
+
       for(File child : childs) {
         explore(child, vistor);
       }
